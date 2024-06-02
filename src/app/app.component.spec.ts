5f085efadb55351dfade5d87dbec43b1c266ef58
KoetjesreepMacBook <koetjesreepmacbook@Bijoyas-MacBook-Air.local>
2024-06-02 12:19:30 +0200

    Use Jest instead of Jasmine

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,19 +8,19 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  test('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'jam-track' title`, () => {
+  test(`should have the 'jam-track' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('jam-track');
   });
 
-  it('should render title', () => {
+  test('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;

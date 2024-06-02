c3192458c204cbb3644cfd55f43ec76887e60393
KoetjesreepMacBook <koetjesreepmacbook@Bijoyas-MacBook-Air.local>
2024-06-02 12:24:52 +0200

    Add @ng/fire package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,25 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+
+export const firebaseConfig = {
+  projectId: "jam-track-2bce1",
+  appId: "1:226481456230:web:73e4ae094b1fc386c699a7",
+  storageBucket: "jam-track-2bce1.appspot.com",
+  apiKey: "AIzaSyDOGiY1ZyYFELU9ucPVfN63jMBt5jKFXVA",
+  authDomain: "jam-track-2bce1.firebaseapp.com",
+  messagingSenderId: "226481456230",
+  measurementId: "G-EMZSY0SVH2"
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+  ]
 };

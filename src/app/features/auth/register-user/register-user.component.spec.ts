57eda965d810b74a149511e24774ac67180ef3cb
KoetjesreepMacBook <koetjesreepmacbook@Bijoyas-MacBook-Air.local>
2024-06-14 22:48:10 +0200

    Register form submit will call AuthService.

--- src/app/features/auth/register-user/register-user.component.spec.ts
+++ src/app/features/auth/register-user/register-user.component.spec.ts
@@ -1,23 +1,96 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterUserComponent } from './register-user.component';
+import { AuthService } from '../auth.service';
+import { of } from 'rxjs';
 
 describe('RegisterUserComponent', () => {
   let component: RegisterUserComponent;
   let fixture: ComponentFixture<RegisterUserComponent>;
+  const authServiceMock = { signUp: jest.fn().mockReturnValue(of('success')) };
+
+  const testData = {
+    username: 'username123',
+    email: 'username123@test.com',
+    password: 'username123@'
+  }
+  const htmlElements = {
+    usernameInput: () => (fixture.debugElement.nativeElement as HTMLElement).querySelector('#usernameInput') as HTMLInputElement,
+    emailInput: () => (fixture.debugElement.nativeElement as HTMLElement).querySelector('#emailInput') as HTMLInputElement,
+    passwordInput: () => (fixture.debugElement.nativeElement as HTMLElement).querySelector('#passwordInput') as HTMLInputElement,
+    submitButton: () => (fixture.debugElement.nativeElement as HTMLElement).querySelector('#submitButton') as HTMLButtonElement,
+    setInput: (htmlInputElement: HTMLInputElement, value: string) => {
+      htmlInputElement.value = value;
+      htmlInputElement.dispatchEvent(new Event('input', { bubbles: true }));
+    }
+  }
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     await TestBed.configureTestingModule({
-      imports: [RegisterUserComponent]
-    })
-    .compileComponents();
-    
+      imports: [RegisterUserComponent],
+      providers: [{ provide: AuthService, useValue: authServiceMock }]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegisterUserComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onSubmit()', () => {
+    test('should write the filled in values to reactive form object', () => {
+      htmlElements.setInput(htmlElements.usernameInput(), testData.username);
+      htmlElements.setInput(htmlElements.emailInput(), testData.email);
+      htmlElements.setInput(htmlElements.passwordInput(), testData.password);
+
+      expect(component.authForm.value).toEqual({ displayName: testData.username, email: testData.email, password: testData.password });
+    });
+
+    test('should request to register through firebase when form valid', () => {
+      htmlElements.setInput(htmlElements.usernameInput(), testData.username);
+      htmlElements.setInput(htmlElements.emailInput(), testData.email);
+      htmlElements.setInput(htmlElements.passwordInput(), testData.password);
+
+      htmlElements.submitButton().click();
+
+      expect(authServiceMock.signUp).toHaveBeenCalledWith(testData.username, testData.email, testData.password);
+    });
+
+    test('should not request to register through firebase when form empty', () => {
+      htmlElements.submitButton().click();
+
+      expect(authServiceMock.signUp).not.toHaveBeenCalled();
+    });
+
+    test('should not request to register through firebase when username empty', () => {
+      htmlElements.setInput(htmlElements.emailInput(), testData.email);
+      htmlElements.setInput(htmlElements.passwordInput(), testData.password);
+      htmlElements.submitButton().click();
+
+      expect(authServiceMock.signUp).not.toHaveBeenCalled();
+    });
+
+    test('should not request to register through firebase when email empty', () => {
+      htmlElements.setInput(htmlElements.usernameInput(), testData.username);
+      htmlElements.setInput(htmlElements.passwordInput(), testData.password);
+
+      htmlElements.submitButton().click();
+
+      expect(authServiceMock.signUp).not.toHaveBeenCalled();
+    });
+
+    test('should not request to register through firebase when email empty', () => {
+      htmlElements.setInput(htmlElements.usernameInput(), testData.username);
+      htmlElements.setInput(htmlElements.emailInput(), testData.email);
+
+      htmlElements.submitButton().click();
+
+      expect(authServiceMock.signUp).not.toHaveBeenCalled();
+    });
   });
 });
--- src/app/features/auth/register-user/register-user.component.ts
+++ src/app/features/auth/register-user/register-user.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BehaviorSubject } from 'rxjs';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register-user',
@@ -17,19 +18,16 @@ export class RegisterUserComponent {
     password: ['', Validators.required]
   });
 
-  private readonly heading$$ = new BehaviorSubject<string>('Register');
-  public readonly heading$ = this.heading$$.asObservable();
 
-  public constructor(private readonly formBuilder: FormBuilder) { }
+  public constructor(private readonly formBuilder: FormBuilder, private readonly authService: AuthService) { }
 
   public onSubmit(): void {
-    console.log(`%c🍟🍔🍕 auth.component.ts[ln:22] onSubmit()`, 'color: deeppink', this.authForm.valid, this.authForm.value);
     if (this.authForm.valid) {
-      // this.authService.signUp(this.authForm.value.displayName!, this.authForm.value.email!, this.authForm.value.password!)
-      //   .subscribe({
-      //     next: (result: string) => console.log(`%c🍟🍔🍕 auth.component.ts[ln:25] next signup`, 'color: deeppink', result),
-      //     error: (e) => console.error(`%c🍟🍔🍕 auth.component.ts[ln:26] error sign up `, e)
-      //   })
+      this.authService.signUp(this.authForm.value.displayName!, this.authForm.value.email!, this.authForm.value.password!)
+        .subscribe({
+          next: (result: string) => console.log(`%c🍟🍔🍕 auth.component.ts[ln:25] next signup`, 'color: deeppink', result),
+          error: (e) => console.error(`%c🍟🍔🍕 auth.component.ts[ln:26] error sign up `, e)
+        });
     }
   }
 }
